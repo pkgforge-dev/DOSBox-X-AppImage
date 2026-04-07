56a67a5ef5469ccaf6a6709f420b6f9ca1f9e888
Link4Electronics <link4electronics@gmail.com>
2026-04-06 23:55:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,6 +8,7 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     libdecor \
+    libxtst  \
     python
 
 echo "Installing debloated packages..."
