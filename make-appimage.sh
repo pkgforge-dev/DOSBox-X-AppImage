5acd97cae9852a54d2041aca10c17a8f274ded5a
Link4Electronics <link4electronics@gmail.com>
2026-05-20 04:36:24 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,13 +11,14 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/dosbox-x.svg
 export DESKTOP=/usr/share/applications/com.dosbox_x.DOSBox-X.desktop
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
+export DEPLOY_PULSE=1
 export DEPLOY_PYTHON=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/dosbox-x /usr/lib/libfluidsynth.so* /usr/lib/libXtst.so*
 
 # Additional changes can be done in between here
+echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
