faf5d3e3c9dd448a20b1eec930180bbcd379f324
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:38:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q dosbox-x-sdl2 | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q dosbox-x-sdl2 | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -16,8 +16,6 @@ export DEPLOY_PYTHON=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/dosbox-x /usr/lib/libfluidsynth.so* /usr/lib/libXtst.so*
-
-# Additional changes can be done in between here
 echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
